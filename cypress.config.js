b037e2183bfc69bfdf4bd191b3eaf51c90d152ba
ngocpham18 <103478481+ngocpham18@users.noreply.github.com>
2023-03-22 02:10:42 +0700

    update

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: 'cwwvp5',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     specPattern: 'cypress/e2e/api/*.js'
-  }
+  },
+  reporter: 'junit',
+  reporterOptions: {
+    mochaFile: 'results/test-results.xml',
+    toConsole: true,
+  },
 });
--- cypress/e2e/api/Test1.js
+++ cypress/e2e/api/Test1.js
@@ -1,33 +1,47 @@
 /// <reference types="Cypress" />
-describe('My First Test Suite', function() {
-    it('Get All Employees', function() {
-        cy.request('https://dummy.restapiexample.com/api/v1/employees')
+describe('Verify API', function() {
+    // it('Get All Employees', function() {
+    //     cy.request('https://dummy.restapiexample.com/api/v1/employees')
+    //         .should((response) => {
+    //             expect(response.status).to.eq(200)
+    //             expect(response).property('body').to.contain({
+    //                 message: 'Successfully! All records has been fetched.',
+    //               })
+    //     })
+    //     cy.wait(3000)
+    // })
+    // it('Get Employee ID', function() {
+    //     cy.request('https://dummy.restapiexample.com/api/v1/employee/3000')
+    //         .should((response) => {
+    //             expect(response.status).to.eq(200)
+    //             expect(response).property('body').to.contain({
+    //                 message: 'Successfully! Record has been fetched.',
+    //             })
+    //     })
+    //     cy.wait(3000)
+    // })
+    // it('Create New Employee', function() {
+    //     cy.request('POST', 'https://dummy.restapiexample.com/api/v1/create', {
+    //         body: '{"name":"test20230321_1","salary":"123","age":"23"}',
+    //     }).then((response) => {
+    //         expect(response.status).to.eq(200)
+    //         expect(response).property('body').to.contain({
+    //             message: 'Successfully! Record has been added.',
+    //         })
+    //     })
+    //     cy.wait(3000)
+    // })
+    it('to get all employees', function() {
+        cy.request('https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty')
             .should((response) => {
                 expect(response.status).to.eq(200)
-                expect(response).property('body').to.contain({
-                    message: 'Successfully! All records has been fetched.',
-                  })
         })
         cy.wait(3000)
     })
-    it('Get Employee ID', function() {
-        cy.request('https://dummy.restapiexample.com/api/v1/employee/3000')
+    it('to get an employee ID', function() {
+        cy.request('https://rahulshettyacademy.com/Library/GetBook.php?AuthorName=shetty')
             .should((response) => {
                 expect(response.status).to.eq(200)
-                expect(response).property('body').to.contain({
-                    message: 'Successfully! Record has been fetched.',
-                })
-        })
-        cy.wait(3000)
-    })
-    it('Create New Employee', function() {
-        cy.request('POST', 'https://dummy.restapiexample.com/api/v1/create', {
-            body: '{"name":"test20230321_1","salary":"123","age":"23"}',
-        }).then((response) => {
-            expect(response.status).to.eq(200)
-            expect(response).property('body').to.contain({
-                message: 'Successfully! Record has been added.',
-            })
         })
         cy.wait(3000)
     })
